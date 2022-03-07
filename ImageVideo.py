417d72dfebce207cc3a14aeafdcf1bdb8acb2823
Joey Paschke <58445029+DuckieOnQuacks@users.noreply.github.com>
2022-03-07 12:12:06 -0800

    created more stream line image to video conversion

--- ImageVideo.py
+++ ImageVideo.py
@@ -1,22 +1,36 @@
 import os
 import cv2
+import moviepy.editor as mp
+import subprocess
 
-def generate_video():
-    image_folder = 'C:\\Users\\jojop\\Desktop\\AI Convert\\OUT'
-    video_name = 'OliverTreeAI.mp4'
-    os.chdir("C:\\Users\\jojop\\Desktop\\AI Convert\\FinalVideo")
 
+def convert_avi_to_mp4():
+    avi_file_path = os.getcwd() + "\\render.avi"
+    output_name = os.getcwd() + "\\render.mp4"
+
+    subprocess.call(["ffmpeg", "-i", avi_file_path,output_name])
+
+
+def audio():
+    inaudio = os.getcwd() + "\\output.mp3"
+    invideo = os.getcwd() + "\\render.mp4"
+    output = os.getcwd() + "\\final_render.mp4"
+    os.system(f"ffmpeg -i {invideo} -i {inaudio} -c copy -map 0:v:0 -map 1:a:0 {output}")
+
+
+def generate_video(framerate):
+    image_folder = "C:\\Users\\jojop\\Desktop\\AI Convert Fast\\Oliver Tree - Cowboys dont cry\\OUT"
     images = [img for img in os.listdir(image_folder)]
-    fourcc = cv2.VideoWriter_fourcc(*'DIVX') 
-    frame = cv2.imread(os.path.join(image_folder, images[0]))
-    video = cv2.VideoWriter(video_name, fourcc, 24, (1024, 1024))
+    images.sort(key = lambda x: int(x.split(".")[0]))
 
-    # Appending the images to the video one by one 
-    for i, image in enumerate(images):
-        video.write(cv2.imread(os.path.join(image_folder,image)))
-
-    # Deallocating memory taken for window creation 
+    video = cv2.VideoWriter("render.avi", 0, framerate, (1024,1024))
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+        #os.remove(image)
     cv2.destroyAllWindows()
-    video.release()  # releasing the video generated 
-    
-generate_video()
+    video.release()
+    #convert_avi_to_mp4()
+    #audio()
+
+if __name__ ==  "__main__":
+    generate_video(24)
